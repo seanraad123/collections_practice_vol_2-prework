654d1b3d1b3599a9550d5f1fdd16826a441747a2
Sean Conrad <seanconrad123@gmail.com>
2018-06-25 04:57:56 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -56,9 +56,9 @@ array1 = [{:name => "blake", :count => 2}, {:name => "ashley", :count => 1}]
 
 def count_elements(words)
     counts = Hash.new 0
-
+    counts[word] = {count: 0}
     words.each do |word|
-      counts[word] = {count: 0}
+      
       counts[word][:count] += 1
     end
     print [counts]
