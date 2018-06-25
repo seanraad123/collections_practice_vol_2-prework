89da8cd20f118fecc7eba3d334fb027334bcb454
Sean Conrad <seanconrad123@gmail.com>
2018-06-25 05:01:45 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -58,6 +58,7 @@ def count_elements(words)
     counts = Hash.new 0
     words.each do |word|
       counts[word] = :count, 1
+      counts[:name].store :style,[style[0]]
     end
     print [counts]
 end
